56caa41e44cf926cc788faa86ae41bf01cef9141
nunomota <nuno.goncalves@epfl.ch>
2016-04-16 21:57:27 +0200

    Creates Transform class to hold an Object3D's world information

---
+++ project/Transform/Transform.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "icg_helper.h"
+#include "glm/gtc/type_ptr.hpp"
+
+class Transform {
+
+    private:
+        glm::vec3 position_(0.0f, 0.0f, 0.0f);
+        glm::vec3 rotation_(0.0f, 0.0f, 0.0f);
+        glm::vec3 scale_(1.0f, 1.0f, 1.0f);
+
+    public:
+        void setPosition(glm::vec3 new_position) {
+            position_ = new_position;
+        }
+
+        void setRotation(glm::vec3 new_rotation) {
+            rotation_ = new_rotation;
+        }
+
+        void setScale(glm::vec3 new_scale) {
+            scale_ = new_scale;
+        }
+
+        glm::vec3 getPosition() {
+            return position_;
+        }
+
+        glm::vec3 getRotation() {
+            return rotation_;
+        }
+
+        glm::vec3 getScale() {
+            return scale_;
+        }
+}
