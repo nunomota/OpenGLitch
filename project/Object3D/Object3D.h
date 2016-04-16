f6bf95f1e79abffe59b3ec1115b997cac5862ad3
nunomota <nuno.goncalves@epfl.ch>
2016-04-16 23:15:02 +0200

    Completes methods and Adds comments

--- project/Object3D/Object3D.h
+++ project/Object3D/Object3D.h
@@ -4,6 +4,12 @@
 
 #include "Reporter/Reporter.h"
 
+Reporter reporter;
+
+/** The Object3D class is a generic class from
+  * which every 3D object instatiated on a scene
+  * inherit from.
+  */
 class Object3D {
 
     protected:
@@ -13,29 +19,37 @@ class Object3D {
         GLuint program_id_;                     // GLSL shader program ID
         GLuint num_indices_;                    // number of vertices to render
 
-        bool is_initialized = false;
+        bool is_initialized_ = false;
+        glm::mat4 model_;
 
         virtual void Init();
 
     public:
         Transform transform;    // Object-specific Transform
 
-        void rotate() {
-            return 0;
+        // method called to rotate an Object3D along the specified axys
+        void rotate(glm::vec3 rotation_vector) {
+            glm::vec3 current_rotation = transform.getRotation();
+            // TODO keep every single coordinate between 0 and 360
+            transform.setRotation(current_rotation + rotation_vector);
         }
 
-        void translate() {
-            return 0;
+        // method called to translate an Object3D according to a vector
+        void translate(glm::vec3 translation_vector) {
+            glm::vec3 current_position = transform.getPosition();
+            transform.setPosition(current_position + translation_vector);
         }
 
-        void scale() {
-            return 0;
+        // method called to scale an Object3D along the specified axys
+        void scale(glm::vec3 scaling_vector) {
+            glm::vec3 current_scale = transform.getScale();
+            transform.setScale(current_scale + scaling_vector);
         }
 
         void Draw(const glm::mat4 &view = IDENTITY_MATRIX,
                   const glm::mat4 &projection = IDENTITY_MATRIX) {
 
-            if (is_initialized) {
+            if (is_initialized_) {
                 glUseProgram(program_id_);
                 glBindVertexArray(vertex_array_id_);
 
