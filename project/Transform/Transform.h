411c9dc95065eaff39b08dce99f10122000b6c47
nunomota <nuno.goncalves@epfl.ch>
2016-04-17 13:53:50 +0200

    Reverts parent changes to Transform

--- project/Transform/Transform.h
+++ project/Transform/Transform.h
@@ -13,7 +13,6 @@ class Transform {
         glm::vec3 rotation_ = glm::vec3(0.0f, 0.0f, 0.0f);
         glm::vec3 scale_ = glm::vec3(1.0f, 1.0f, 1.0f);
 
-        Transform parent_ = NULL;
         glm::mat4 model_ = IDENTITY_MATRIX;
 
         void recalculateModelMatrix() {
@@ -27,11 +26,6 @@ class Transform {
 
             // apply translation
             model_ = glm::translate(model_, position_);
-
-            // apply parent's calculation
-            if (parent_ != NULL) {
-                model_ = model_ * parent_.getModelMatrix();
-            }
         }
 
     public:
@@ -50,10 +44,6 @@ class Transform {
             recalculateModelMatrix();
         }
 
-        void setParent(Transform parent_transform) {
-            if (parent_transform != NULL) parent_ = parent_transform;
-        }
-
         glm::vec3 getPosition() {
             return position_;
         }
@@ -68,9 +58,5 @@ class Transform {
 
         glm::mat4 getModelMatrix() {
             return model_;
-        }
-
-        Transform getParent() {
-            return parent_;
         }
 };
