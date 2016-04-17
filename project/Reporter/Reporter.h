9809fdffb6ba49765779ad3e677325af94e9f2f2
nunomota <nuno.goncalves@epfl.ch>
2016-04-17 15:34:48 +0200

    Removes methods implemented by GlmStrings

--- project/Reporter/Reporter.h
+++ project/Reporter/Reporter.h
@@ -2,11 +2,15 @@
 #include "icg_helper.h"
 #include "glm/gtc/type_ptr.hpp"
 
+#include "../GlmStrings/GlmStrings.h"
+
 /** This class is used to print any message
   * with a specific flag (warning, error, etc).
   */
 class Reporter {
     private:
+        GlmStrings glm_strings_;
+
         const std::string default_flag_ = "Info";
         const char flag_delimiters[2]{'[', ']'};
 
@@ -16,51 +20,17 @@ class Reporter {
             std::cout << final_message.str() << std::endl;
         }
 
-        std::string glm_to_string(glm::vec3 vector) {
-            std::ostringstream string_representation;
-            string_representation << "vec3(" << std::fixed << std::setprecision(2)
-                                  << vector.x << ", "
-                                  << vector.y << ", "
-                                  << vector.z 
-                                  << ")";
-            return string_representation.str();
-        }
-
-        std::string glm_to_string(glm::mat4 matrix) {
-            std::ostringstream string_representation;
-            string_representation << "Mat4" << std::endl << std::fixed << std::setprecision(2);
-            for (int x = 0; x < 4; x++) {
-                string_representation << "[";
-                for (int y = 0; y < 4; y++) {
-                    float element = float(matrix[x][y]);
-                    if (y < 3) string_representation << pretty_float(element) << ", ";
-                    else string_representation << pretty_float(element);
-                }
-                if (x < 3) string_representation << "]" << std::endl;
-                else string_representation << "]";
-            }
-            return string_representation.str();
-        }
-
-        std::string pretty_float(float number) {
-            std::ostringstream number_representation;
-            number_representation << std::fixed << std::setprecision(2);
-            if (number >= 0) number_representation << "+" << number;
-            else number_representation << number;
-            return number_representation.str();
-        }
-
     public:
         void println(std::string message, std::string flag) {
             output(message, flag);
         }
 
         void println(glm::vec3 vector, std::string flag) {
-            output(glm_to_string(vector), flag);
+            output(glm_strings_.create(vector), flag);
         }
 
         void println(glm::mat4 matrix, std::string flag) {
-            output(glm_to_string(matrix), flag);
+            output(glm_strings_.create(matrix), flag);
         }
 
         void println(std::string message) {
@@ -68,11 +38,11 @@ class Reporter {
         }
 
         void println(glm::vec3 vector) {
-            output(glm_to_string(vector), default_flag_);
+            output(glm_strings_.create(vector), default_flag_);
         }
 
         void println(glm::mat4 matrix) {
-            output(glm_to_string(matrix), default_flag_);
+            output(glm_strings_.create(matrix), default_flag_);
         }
 
         void print_special(std::string message) {
