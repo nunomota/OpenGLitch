547d681189e1259e08d0c36d83ed0aa45d66947b
nunomota <nuno.goncalves@epfl.ch>
2016-04-18 15:45:58 +0200

    Moves code relative to physical objects to subclass

--- project/Object3D/Object3D.h
+++ project/Object3D/Object3D.h
@@ -7,10 +7,9 @@
   * inherit from.
   */
 class Object3D {
-    private:
-        Reporter reporter_;
-
     protected:
+        Reporter reporter_;
+        
         GLuint vertex_array_id_;                // vertex array object
         GLuint vertex_buffer_object_position_;  // memory buffer for positions
         GLuint vertex_buffer_object_index_;     // memory buffer for indices
@@ -55,71 +54,10 @@ class Object3D {
             return transform;
         }
 
-        void Init() {
-            // call to the sub-class' method to get the shaders' name
-            LoadShaders();
+        virtual void Init() {}
 
-            if(!program_id_) {
-                exit(EXIT_FAILURE);
-            }
+        virtual void Draw(const glm::mat4 &view = IDENTITY_MATRIX,
+                  const glm::mat4 &projection = IDENTITY_MATRIX) {}
 
-            glUseProgram(program_id_);
-
-            // vertex one vertex array
-            glGenVertexArrays(1, &vertex_array_id_);
-            glBindVertexArray(vertex_array_id_);
-
-            // call to sub-class' method to setup the vertices correctly
-            SetupVertices();
-
-            // position shader attribute
-            // fetch attribute ID for vertex positions
-            GLuint loc_position = glGetAttribLocation(program_id_, "position");
-            glEnableVertexAttribArray(loc_position); // Enable it
-            glVertexAttribPointer(loc_position, 3, GL_FLOAT, DONT_NORMALIZE,
-                                  ZERO_STRIDE, ZERO_BUFFER_OFFSET);
-
-            // call to sub-class' method to setup the index buffer correctly
-            SetupIndexBuffer();
-
-            // other uniforms
-            MVP_id_ = glGetUniformLocation(program_id_, "MVP");
-
-            // to avoid the current object being polluted
-            glBindVertexArray(0);
-            glUseProgram(0);
-
-            is_initialized_ = true;
-        }
-
-        void Draw(const glm::mat4 &view = IDENTITY_MATRIX,
-                  const glm::mat4 &projection = IDENTITY_MATRIX) {
-
-            if (is_initialized_) {
-                glUseProgram(program_id_);
-                glBindVertexArray(vertex_array_id_);
-
-                // setup MVP
-                glm::mat4 model = transform.getModelMatrix();
-                glm::mat4 MVP = projection*view*model;
-                glUniformMatrix4fv(MVP_id_, ONE, DONT_TRANSPOSE, glm::value_ptr(MVP));
-
-                // draw
-                glDrawElements(GL_TRIANGLES, num_indices_, GL_UNSIGNED_INT, 0);
-
-                glBindVertexArray(0);
-                glUseProgram(0);
-            } else {
-                reporter_.println("Cannot draw uninitialized Object3D!", "Warning");
-            }
-        }
-
-        void Cleanup() {
-            glBindVertexArray(0);
-            glUseProgram(0);
-            glDeleteBuffers(1, &vertex_buffer_object_position_);
-            glDeleteBuffers(1, &vertex_buffer_object_index_);
-            glDeleteProgram(program_id_);
-            glDeleteVertexArrays(1, &vertex_array_id_);
-        }
+        virtual void Cleanup() {}
 };
---
+++ project/Object3D/PhysicalObject/PhysicalObject.h
@@ -0,0 +1,77 @@
+#pragma once
+#include "icg_helper.h"
+#include "glm/gtc/type_ptr.hpp"
+
+class PhysicalObject: public Object3D {
+    protected:
+        virtual void LoadShaders() {};
+        virtual void SetupVertices() {};
+        virtual void SetupIndexBuffer() {};
+
+    public:
+        void Init() {
+            // call to the sub-class' method to get the shaders' name
+            LoadShaders();
+
+            if(!program_id_) {
+                exit(EXIT_FAILURE);
+            }
+
+            glUseProgram(program_id_);
+
+            // vertex one vertex array
+            glGenVertexArrays(1, &vertex_array_id_);
+            glBindVertexArray(vertex_array_id_);
+
+            // call to sub-class' method to setup the vertices correctly
+            SetupVertices();
+
+            // position shader attribute
+            // fetch attribute ID for vertex positions
+            GLuint loc_position = glGetAttribLocation(program_id_, "position");
+            glEnableVertexAttribArray(loc_position); // Enable it
+            glVertexAttribPointer(loc_position, 3, GL_FLOAT, DONT_NORMALIZE,
+                                  ZERO_STRIDE, ZERO_BUFFER_OFFSET);
+
+            // call to sub-class' method to setup the index buffer correctly
+            SetupIndexBuffer();
+
+            // other uniforms
+            MVP_id_ = glGetUniformLocation(program_id_, "MVP");
+
+            // to avoid the current object being polluted
+            glBindVertexArray(0);
+            glUseProgram(0);
+
+            is_initialized_ = true;
+        }
+
+        void Draw() {
+            if (is_initialized_) {
+                glUseProgram(program_id_);
+                glBindVertexArray(vertex_array_id_);
+
+                // setup MVP
+                glm::mat4 model = transform.getModelMatrix();
+                glm::mat4 MVP = projection*view*model;
+                glUniformMatrix4fv(MVP_id_, ONE, DONT_TRANSPOSE, glm::value_ptr(MVP));
+
+                // draw
+                glDrawElements(GL_TRIANGLES, num_indices_, GL_UNSIGNED_INT, 0);
+
+                glBindVertexArray(0);
+                glUseProgram(0);
+            } else {
+                reporter_.println("Cannot draw uninitialized Object3D!", "Warning");
+            }
+        }
+
+        void Cleanup() {
+            glBindVertexArray(0);
+            glUseProgram(0);
+            glDeleteBuffers(1, &vertex_buffer_object_position_);
+            glDeleteBuffers(1, &vertex_buffer_object_index_);
+            glDeleteProgram(program_id_);
+            glDeleteVertexArrays(1, &vertex_array_id_);
+        }
+};
