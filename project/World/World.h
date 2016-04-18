9035cf8b22a8989b2e623119c27f25b7a8589b82
nunomota <nuno.goncalves@epfl.ch>
2016-04-18 15:59:45 +0200

    Refactors every static component of World

--- project/World/World.h
+++ project/World/World.h
@@ -4,41 +4,41 @@
 
 class World {
     private:
-        static std::vector<Object3D> uninitialized;
-        static std::vector<Object3D> objects;
-        static Camera main_camera;
+        std::vector<Object3D> uninitialized;
+        std::vector<Object3D> objects;
+        Camera main_camera;
 
     public:
-        static void addObject(Object3D new_object) {
+        void addObject(Object3D new_object) {
             if (new_object != NULL) uninitialized.pushBack(new_object);
         }
 
-        static void removeObject(Object3D target_object) {
+        void removeObject(Object3D target_object) {
             // TODO remove target_object from LL
         }
 
-        static void setCamera(Camera camera) {
+        void setCamera(Camera camera) {
             if (camera != NULL) main_camera = camera;
         }
 
-        static Camera getCamera() {
+        Camera getCamera() {
             return main_camera;
         }
 
-        static void Init() {
+        void Init() {
             for (std::vector<Object3D>::iterator it = uninitialized.begin(); it != uninitialized.end(); ++it) {
                 it->Init();
                 // TODO remove it from 'uninitialized' and add it to 'objects'
             }
         }
 
-        static void Draw() {
+        void Draw() {
             for (std::vector<Object3D>::iterator it = objects.begin(); it != objects.end(); ++it) {
                 it->Draw(main_camera.getViewMatrix(), main_camera.getProjectionMatrix());
             }
         }
 
-        static void Cleanup() {
+        void Cleanup() {
             for (std::vector<Object3D>::iterator it = objects.begin(); it != objects.end(); ++it) {
                 it->Cleanup();
             }
