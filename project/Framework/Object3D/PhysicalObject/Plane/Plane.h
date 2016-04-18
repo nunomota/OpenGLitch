468893c4012d228735b8d52c5e7c834fbc464e9a
nunomota <nuno.goncalves@epfl.ch>
2016-04-18 20:38:05 +0200

    Implements plane correctly and with right shader

--- project/Framework/Object3D/PhysicalObject/Plane/Plane.h
+++ project/Framework/Object3D/PhysicalObject/Plane/Plane.h
@@ -4,62 +4,34 @@
 
 class Plane: public PhysicalObject {
     private:
-        const int sub_division = 512;
+        // Position buffer
+        const GLfloat position[12] = {-1.0f, 0.0f,  +1.0f,
+                                      +1.0f, 0.0f,  +1.0f,
+                                      -1.0f, 0.0f,  -1.0f,
+                                      +1.0f, 0.0f,  -1.0f};
 
-        std::vector<GLfloat> vertices;
-        std::vector<GLuint> indices;
-
-        void addVertex(float x, float y, std::vector<GLfloat>* vertices) {
-            (*vertices).push_back(x); (*vertices).push_back(y);
-        }
-
-        void useVertex(int index, std::vector<GLuint>* indices) {
-            (*indices).push_back(index);
-        }
-
-        void calculateVertices() {
-            float step = 2.0f / (float)sub_division;
-            for (int i = 0, mult = 1; i < sub_division+1; i++, mult = -mult) {
-                float x_coord = -1.0f + i*step;
-                for (int j = 0; j < sub_division+1; j++) {
-                    float y_coord = (float)mult * (-1.0f + j*step);
-                    if (i == 0) {
-                        addVertex(x_coord, y_coord, &vertices); 
-                    } else {
-                        int index = (int)vertices.size()/2;
-
-                        // use the previously calculated vertex, in the same line
-                        useVertex(index - (j*2+1), &indices);
-                        // create a new vertex
-                        addVertex(x_coord, y_coord, &vertices);
-                        useVertex(index, &indices);
-                    }
-                }
-            }
-        }
+        // index buffer
+        const GLuint index[36] = {0, 1, 2,
+                                3, 2, 1};
 
     protected:
         void LoadShaders() {
             // compile the shaders.
-            program_id_ = icg_helper::LoadShaders("plane_vshader.glsl",
-                                                  "plane_fshader.glsl");
+            program_id_ = icg_helper::LoadShaders("cube_vshader.glsl",
+                                                  "cube_fshader.glsl");
         }
 
         void SetupVertices() {
-            calculateVertices();
-
             glGenBuffers(1, &vertex_buffer_object_position_);
             glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object_position_);
-            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat),
-                         &vertices[0], GL_STATIC_DRAW);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(position), position, GL_STATIC_DRAW);
         }
 
         void SetupIndexBuffer() {
-            num_indices_ = indices.size();
+            num_indices_ = sizeof(index) / sizeof(GLuint);
 
             glGenBuffers(1, &vertex_buffer_object_index_);
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertex_buffer_object_index_);
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint),
-                         &indices[0], GL_STATIC_DRAW);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
         }
 };
--- project/Framework/Object3D/PhysicalObject/Plane/plane_fshader.glsl
+++ project/Framework/Object3D/PhysicalObject/Plane/plane_fshader.glsl
@@ -1,16 +1,11 @@
 #version 330
 
+in vec2 uv;
+
 out vec3 color;
 
-// color buffer
-const vec3 COLOR[6] = vec3[](
-    vec3(0.0, 0.0, 1.0),
-    vec3(0.0, 1.0, 0.0),
-    vec3(0.0, 1.0, 1.0),
-    vec3(1.0, 0.0, 0.0),
-    vec3(1.0, 0.0, 1.0),
-    vec3(1.0, 1.0, 0.0));
+uniform sampler2D tex;
 
 void main() {
-    color = COLOR[gl_PrimitiveID / 2];
+    color = texture(tex, uv).rgb;
 }
