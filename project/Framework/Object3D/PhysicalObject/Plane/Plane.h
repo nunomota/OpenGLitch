fbe8558a2b4568804feb97ad9024f2616c275fea
nunomota <nuno.goncalves@epfl.ch>
2016-04-18 20:08:00 +0200

    Refacters Plane class

--- project/Framework/Object3D/PhysicalObject/Plane/Plane.h
+++ project/Framework/Object3D/PhysicalObject/Plane/Plane.h
@@ -4,48 +4,62 @@
 
 class Cube: public PhysicalObject {
     private:
-        // Position buffer
-        const GLfloat position[24] = {-1.0f, -1.0f,  1.0f, // left, bottom, front
-                                    1.0f, -1.0f,  1.0f,  // right, bottom, front
-                                    1.0f,  1.0f,  1.0f,  // right, top, front
-                                    -1.0f,  1.0f,  1.0f, // left, top, front
-                                    -1.0f, -1.0f, -1.0f, // left, bottom, back
-                                    1.0f, -1.0f, -1.0f,  // right, bottom, back
-                                    1.0f,  1.0f, -1.0f,  // right, top, back
-                                    -1.0f,  1.0f, -1.0f};// left, top, back
+        const int sub_division = 512;
 
-        // index buffer
-        const GLuint index[36] = {0, 1, 2,  // front face triangle 1
-                                0, 2, 3,  // front face triangle 2
-                                1, 5, 6,  // right face triangle 1
-                                1, 6, 2,  // right face triangle 2
-                                5, 4, 7,  // back face triangle 1
-                                5, 7, 6,  // back face triangle 2
-                                4, 0, 3,  // left face triangle 1
-                                4, 3, 7,  // left face triangle 2
-                                3, 2, 6,  // top face triangle 1
-                                3, 6, 7,  // top face triangle 2
-                                1, 0, 4,  // bottom face triangle 1
-                                1, 4, 5}; // bottom face triangle 2
+        std::vector<GLfloat> vertices;
+        std::vector<GLuint> indices;
+
+        void addVertex(float x, float y, std::vector<GLfloat>* vertices) {
+            (*vertices).push_back(x); (*vertices).push_back(y);
+        }
+
+        void useVertex(int index, std::vector<GLuint>* indices) {
+            (*indices).push_back(index);
+        }
+
+        void calculateVertices() {
+            float step = 2.0f / (float)sub_division;
+            for (int i = 0, mult = 1; i < sub_division+1; i++, mult = -mult) {
+                float x_coord = -1.0f + i*step;
+                for (int j = 0; j < sub_division+1; j++) {
+                    float y_coord = (float)mult * (-1.0f + j*step);
+                    if (i == 0) {
+                        addVertex(x_coord, y_coord, &vertices); 
+                    } else {
+                        int index = (int)vertices.size()/2;
+
+                        // use the previously calculated vertex, in the same line
+                        useVertex(index - (j*2+1), &indices);
+                        // create a new vertex
+                        addVertex(x_coord, y_coord, &vertices);
+                        useVertex(index, &indices);
+                    }
+                }
+            }
+        }
 
     protected:
         void LoadShaders() {
             // compile the shaders.
-            program_id_ = icg_helper::LoadShaders("cube_vshader.glsl",
-                                                  "cube_fshader.glsl");
+            program_id_ = icg_helper::LoadShaders("plane_vshader.glsl",
+                                                  "plane_fshader.glsl");
         }
 
         void SetupVertices() {
+            calculateVertices();
+            
             glGenBuffers(1, &vertex_buffer_object_position_);
             glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object_position_);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(position), position, GL_STATIC_DRAW);
+            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat),
+                         &vertices[0], GL_STATIC_DRAW);
         }
 
         void SetupIndexBuffer() {
-            num_indices_ = sizeof(index) / sizeof(GLuint);
+            num_indices_ = indices.size();
 
             glGenBuffers(1, &vertex_buffer_object_index_);
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertex_buffer_object_index_);
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint),
+                         &indices[0], GL_STATIC_DRAW);
         }
 };
