e327d14dd5a90a1a6bb721a628fa6e1fe2d0f4d2
Pherresa <pablo.herrerosanz@epfl.ch>
2016-04-19 20:02:22 +0200

    Makes minor fixes

--- project/grid/grid_fshader.glsl
+++ project/grid/grid_fshader.glsl
@@ -8,32 +8,30 @@ vec3 Ld = vec3(1.0f, 1.0f, 1.0f);
 vec3 kd = vec3(0.9f, 0.5f, 0.5f);
 uniform sampler2D height_tex;
 
-float iterations = 2;
+int iterations = 1;
 vec3 normal_mv = vec3 (0.0f,0.0f,0.0f);
 
 void main() {
-    int total = 0;
+    int max_i = iterations * 2 + 1;
+    int max_j = max_i;
+    vec2 bl_corner = vec2(uv.x - float(iterations), uv.y - float(iterations));
     //looping to get the neighbors coordinates ((x-z, y+z), (x+z, y+z) and so on...
-    for (int i = 0; i<= iterations * 2 + 1; i++){
-        for(int j = 0; i<= iterations * 2 + 1; j++){
-            vec2 neighbor_2D = vec2 (((uv.x - iterations) + uv.x) + i,
-                                     ((uv.y - iterations) + uv.y) + j);
-
+    for (int i = 0; i<= max_i; i++){
+        for(int j = 0; i<= max_j; j++){
+            vec2 neighbor_2D = bl_corner + vec2(float(i), float(j));
             //getting the neighbors height
             float height = texture(height_tex, neighbor_2D).x;
 
             //getting the position of the neighbor
             vec3 neighbor_3D = vec3(neighbor_2D.x, neighbor_2D.y, height);
             normal_mv += neighbor_3D;
-            total++;
         }
     }
 
 
+    normal_mv = normal_mv/(max_i*max_j);
 
-
-
-    normal_mv = normal_mv/total;
+    //normal_mv = vec3(uv.x, uv.y, texture(height_tex, uv).x);
     vec3 n = normalize(normal_mv);
 
     vec3 light_dir = (n - CAMERA_POSITION);
