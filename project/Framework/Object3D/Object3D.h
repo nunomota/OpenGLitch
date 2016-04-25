be1a87267212f8b07d5872499618ff3f3eff58bd
nunomota <nuno.goncalves@epfl.ch>
2016-04-25 19:34:47 +0200

    Adds Object3D to World upon initialization

--- project/Framework/Object3D/Object3D.h
+++ project/Framework/Object3D/Object3D.h
@@ -24,7 +24,7 @@ class Object3D {
     public:
         // constructor
         Object3D () {
-            // TODO add this object to World
+            World.addObject(this);
         }
 
         // method called to rotate an Object3D along the specified axys
