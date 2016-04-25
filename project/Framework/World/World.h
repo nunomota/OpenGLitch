f465274c5fb9adf17b5350be45d67b9a37253575
nunomota <nuno.goncalves@epfl.ch>
2016-04-25 19:30:51 +0200

    Fixes World class

--- project/Framework/World/World.h
+++ project/Framework/World/World.h
@@ -4,44 +4,45 @@
 
 class World {
     private:
-        std::vector<Object3D> uninitialized;
-        std::vector<Object3D> objects;
-        Camera main_camera;
+        static std::vector<Object3D*> uninitialized;
+        static std::vector<Object3D*> objects;
+        static Camera* main_camera;
 
     public:
-        void addObject(Object3D new_object) {
-            if (new_object != NULL) uninitialized.pushBack(new_object);
+        static void addObject(Object3D* new_object) {
+            if (new_object != NULL) uninitialized.push_back(new_object);
         }
 
-        void removeObject(Object3D target_object) {
+        static void removeObject(Object3D* target_object) {
             // TODO remove target_object from LL
         }
 
-        void setCamera(Camera camera) {
+        static void setCamera(Camera* camera) {
             if (camera != NULL) main_camera = camera;
         }
 
-        Camera getCamera() {
+        static Camera* getCamera() {
             return main_camera;
         }
 
-        void Init() {
-            for (std::vector<Object3D>::iterator it = uninitialized.begin(); it != uninitialized.end(); ++it) {
-                it->Init();
-                // TODO remove it from 'uninitialized' and add it to 'objects'
+        static void Init() {
+            for (std::vector<Object3D*>::iterator it = uninitialized.begin(); it != uninitialized.end(); ++it) {
+                (*it)->Init();
+                objects.push_back(*it);
+            }
+            uninitialized.clear();
+        }
+
+        static void Draw() {
+            for (std::vector<Object3D*>::iterator it = objects.begin(); it != objects.end(); ++it) {
+                (*it)->Draw(main_camera->getViewMatrix(), main_camera->getProjectionMatrix());
             }
         }
 
-        void Draw() {
-            for (std::vector<Object3D>::iterator it = objects.begin(); it != objects.end(); ++it) {
-                it->Draw(main_camera.getViewMatrix(), main_camera.getProjectionMatrix());
+        static void Cleanup() {
+            for (std::vector<Object3D*>::iterator it = objects.begin(); it != objects.end(); ++it) {
+                (*it)->Cleanup();
             }
-        }
-
-        void Cleanup() {
-            for (std::vector<Object3D>::iterator it = objects.begin(); it != objects.end(); ++it) {
-                it->Cleanup();
-            }
-            main_camera.Cleanup();
+            main_camera->Cleanup();
         }
 };
