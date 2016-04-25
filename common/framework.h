db3663f0bf268bbca99441a29d1de59b65d2fc9c
nunomota <nuno.goncalves@epfl.ch>
2016-04-25 19:08:39 +0200

    Edits header

--- common/framework.h
+++ common/framework.h
@@ -9,14 +9,14 @@
 --------------------*/
 #include "../project/Framework/Object3D/Transform/Transform.h"
 #include "../project/Framework/Object3D/Object3D.h"
+#include "../project/Framework/Object3D/PhysicalObject/PhysicalObject.h"
+#include "../project/Framework/Object3D/EmptyObject/EmptyObject.h"
 
 // Physical objects
-#include "../project/Framework/Object3D/PhysicalObject/PhysicalObject.h"
 #include "../project/Framework/Object3D/PhysicalObject/Cube/Cube.h"
 #include "../project/Framework/Object3D/PhysicalObject/Plane/Plane.h"
 
 // Empty objects
-#include "../project/Framework/Object3D/EmptyObject/EmptyObject.h"
 #include "../project/Framework/Object3D/EmptyObject/Camera/Camera.h"
 #include "../project/Framework/Object3D/EmptyObject/Light/Light.h"
 #include "../project/Framework/Object3D/EmptyObject/Light/PointLight/PointLight.h"
