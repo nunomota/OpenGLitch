46b051ac61a2482de0df83eebc562ab3f0ba5167
nunomota <nuno.goncalves@epfl.ch>
2016-04-25 19:00:24 +0200

    Fixes private variable access attempt

--- project/Framework/Object3D/EmptyObject/Light/DirectionalLight/DirectionalLight.h
+++ project/Framework/Object3D/EmptyObject/Light/DirectionalLight/DirectionalLight.h
@@ -9,9 +9,9 @@ class DirectionalLight: public Light {
         glm::mat4 getRotationMatrix() {
             glm::mat4 rotation_matrix = IDENTITY_MATRIX;
 
-            rotation_matrix = glm::rotate(rotation_matrix, glm::radians(transform.rotation_.x), glm::vec3(1.0f, 0.0f, 0.0f));
-            rotation_matrix = glm::rotate(rotation_matrix, glm::radians(transform.rotation_.y), glm::vec3(0.0f, 1.0f, 0.0f));
-            rotation_matrix = glm::rotate(rotation_matrix, glm::radians(transform.rotation_.z), glm::vec3(0.0f, 0.0f, 1.0f));
+            rotation_matrix = glm::rotate(rotation_matrix, glm::radians(transform.getRotation().x), glm::vec3(1.0f, 0.0f, 0.0f));
+            rotation_matrix = glm::rotate(rotation_matrix, glm::radians(transform.getRotation().y), glm::vec3(0.0f, 1.0f, 0.0f));
+            rotation_matrix = glm::rotate(rotation_matrix, glm::radians(transform.getRotation().z), glm::vec3(0.0f, 0.0f, 1.0f));
 
             return rotation_matrix;
         }
