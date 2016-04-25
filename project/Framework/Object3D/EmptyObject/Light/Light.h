c32135ab8168dc7e547b62b31749d319c5c3345a
nunomota <nuno.goncalves@epfl.ch>
2016-04-25 18:22:13 +0200

    Adds generic Light properties

---
+++ project/Framework/Object3D/EmptyObject/Light/Light.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "icg_helper.h"
+#include "glm/gtc/type_ptr.hpp"
+
+class Light {
+    protected:
+        glm::vec3 color_ = glm::vec3(0.0f, 0.0f, 0.0f);
+        float intensity_ = 1.0f;
+    public:
+        void setColor(glm::vec3 new_color) {
+            color_ = new_color;
+        }
+
+        void setIntensity(float new_intesity) {
+            intensity_ = new_intesity;
+        }
+
+        glm::vec3 getColor() {
+            return color_;
+        }
+
+        float getIntensity() {
+            return intensity_;
+        }
+};
