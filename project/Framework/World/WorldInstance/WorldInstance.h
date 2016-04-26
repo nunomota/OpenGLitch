570ec0af6d16bc8a57218f1dd50f2b13f152ae90
nunomota <nuno.goncalves@epfl.ch>
2016-04-26 16:29:58 +0200

    Adds subclass of World

---
+++ project/Framework/World/WorldInstance/WorldInstance.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "icg_helper.h"
+#include "glm/gtc/type_ptr.hpp"
+
+class WorldInstance: public World {
+    protected:
+
+        // method called only once
+        void Start() {
+
+        }
+
+        // method called every frame
+        void Update() {
+
+        }    
+}
