3b6ec69b33844fd0376fe230d2a7c4063e58af5c
nunomota <nuno.goncalves@epfl.ch>
2016-04-26 17:24:35 +0200

    Implements Mouse class

--- project/Framework/Input/Mouse/Mouse.h
+++ project/Framework/Input/Mouse/Mouse.h
@@ -3,5 +3,28 @@
 #include "glm/gtc/type_ptr.hpp"
 
 class Mouse {
-    
+    private:
+        const int supportedButtons = 2;
+        bool buttons[supportedButtons];
+
+        void initButtonValues() {
+            for (int i = 0; i < supportedButtons; i++) {
+                buttons[i] = false;
+            }
+        }
+
+    public:
+        static const int LEFT_BUTTON = 0;
+        static const int RIGHT_BUTTON = 1;
+
+        Mouse() {
+            initButtonValues();
+        }
+
+        void setState(int key, bool state) {
+            int new_key = key-1;
+            if (new_key >= 0 && new_key < supportedButtons) {
+                buttons[key] = state;
+            }
+        }
 };
