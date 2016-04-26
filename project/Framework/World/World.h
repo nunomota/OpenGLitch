709484be53ae260fce92537b149b599afc2f7af6
nunomota <nuno.goncalves@epfl.ch>
2016-04-26 15:27:32 +0200

    Refactors World class for readability

--- project/Framework/World/World.h
+++ project/Framework/World/World.h
@@ -10,9 +10,43 @@ class World {
 
         static bool areObjectsUninitialized;
 
+        static void initializeObjects() {
+            for (std::vector<Object3D*>::iterator it = uninitialized.begin(); it != uninitialized.end(); ++it) {
+                Object3D* object = (*it);
+                if(object) {
+                    object->Init();
+                    objects.push_back(object);
+                }
+            }
+            uninitialized.clear();
+            areObjectsUninitialized = false;
+        }
+
+        static void drawObjects() {
+            for (std::vector<Object3D*>::iterator it = objects.begin(); it != objects.end(); ++it) {
+                Object3D* object = (*it);
+                if(object) {
+                    object->Draw(main_camera->getViewMatrix(), main_camera->getProjectionMatrix());
+                }
+            }
+        }
+
+        static void cleanupObjects() {
+            for (std::vector<Object3D*>::iterator it = objects.begin(); it != objects.end(); ++it) {
+                Object3D* object = (*it);
+                if (object) object->Cleanup();
+            }
+            if (main_camera) main_camera->Cleanup();
+            uninitialized.clear();
+            objects.clear();
+        }
+
     public:
         static void addObject(Object3D* new_object) {
-            if (new_object) uninitialized.push_back(new_object);
+            if (new_object) {
+                uninitialized.push_back(new_object);
+                areObjectsUninitialized = true;
+            }
         }
 
         static void removeObject(Object3D* target_object) {
@@ -27,31 +61,15 @@ class World {
             return main_camera;
         }
 
-        static void Init() {
-            for (std::vector<Object3D*>::iterator it = uninitialized.begin(); it != uninitialized.end(); ++it) {
-                Object3D* object = (*it);
-                if(object) {
-                    object->Init();
-                    objects.push_back(object);
-                }
-            }
-            uninitialized.clear();
+        static void Start() {
+            areObjectsUninitialized = false;
         }
 
-        static void Draw() {
-            for (std::vector<Object3D*>::iterator it = objects.begin(); it != objects.end(); ++it) {
-                Object3D* object = (*it);
-                if(object) {
-                    object->Draw(main_camera->getViewMatrix(), main_camera->getProjectionMatrix());
-                }
-            }
+        static void Update() {
+            if (areObjectsUninitialized) initializeObjects(); 
         }
 
-        static void Cleanup() {
-            for (std::vector<Object3D*>::iterator it = objects.begin(); it != objects.end(); ++it) {
-                Object3D* object = (*it);
-                if (object) object->Cleanup();
-            }
-            if (main_camera) main_camera->Cleanup();
+        static void Terminate() {
+            cleanupObjects();
         }
 };
