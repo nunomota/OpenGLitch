3acd8209b7b606b2fc16e2f31a53ecac4cd9a74c
nunomota <nuno.goncalves@epfl.ch>
2016-04-27 14:44:02 +0200

    Adds simple 3D world example

--- project/Framework/World/WorldInstance/WorldInstance.h
+++ project/Framework/World/WorldInstance/WorldInstance.h
@@ -8,9 +8,26 @@ class WorldInstance: public World {
         // method called only once
         void Start() {
             Reporter::println("Start method called");
+            Camera* camera = instantiate(new Camera(45.0f, 1.0f, 0.1f, 100.0f));
             Cube* cube = instantiate(new Cube());
-            //(*cube).translate(glm::vec3(0.0f, 0.0f, -4.0f));
-            //(*cube).rotate(glm::vec3(0.0f, 45.0f, 45.0f));
+            Cube* cube2 = instantiate(new Cube());
+            Cube* cube3 = instantiate(new Cube());
+            setCamera(camera);
+
+            camera->translate(glm::vec3(0.0f, 0.0f, 8.0f));
+            camera->scale(glm::vec3(-0.2f, -0.2f, -0.2f));
+
+            cube->scale(glm::vec3(-0.5f, -0.5f, -0.5f));
+            cube->translate(glm::vec3(0.0f, 0.0f, -2.0f));
+            cube->rotate(glm::vec3(45.0f, 45.0f, 0.0f));
+
+            cube2->scale(glm::vec3(-0.5f, -0.5f, -0.5f));
+            cube2->translate(glm::vec3(-2.0f, 0.0f, -2.0f));
+            cube2->rotate(glm::vec3(-45.0f, 0.0f, 0.0f));
+
+            cube3->scale(glm::vec3(-0.5f, -0.5f, -0.5f));
+            cube3->translate(glm::vec3(2.0f, 0.0f, -2.0f));
+            cube3->rotate(glm::vec3(45.0f, 0.0f, 0.0f));
         }
 
         // method called every frame
