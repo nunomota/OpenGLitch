ab6381433b840cc2e6c160658f86966728f55e9e
Pherresa <pablo.herrerosanz@epfl.ch>
2016-04-27 16:02:21 +0200

    Added some color schema

--- project/grid/grid_fshader.glsl
+++ project/grid/grid_fshader.glsl
@@ -5,20 +5,22 @@ in vec2 uv;
 out vec3 color;
 
 vec3 Ld = vec3(1.0f, 1.0f, 1.0f);
-vec3 kd = vec3(0.9f, 0.5f, 0.5f);
+vec3 kd = vec3(1.0f, 1.0f, 1.0f);
 uniform sampler2D height_tex;
 
-int iterations = 1;
+int iterations = 15;
 vec3 normal_mv = vec3 (0.0f,0.0f,0.0f);
 
 void main() {
     int max_i = iterations * 2 + 1;
     int max_j = max_i;
-    vec2 bl_corner = vec2(uv.x - float(iterations), uv.y - float(iterations));
+    ivec2 tex_size = ivec2(textureSize(height_tex, 0));
+    //vec2 bl_corner = vec2(uv.x - (1/tex_size.x), uv.y - (1/tex_size.y));
+    vec2 bl_corner = uv - (iterations *(1/tex_size));
     //looping to get the neighbors coordinates ((x-z, y+z), (x+z, y+z) and so on...
     for (int i = 0; i<= max_i; i++){
-        for(int j = 0; i<= max_j; j++){
-            vec2 neighbor_2D = bl_corner + vec2(float(i), float(j));
+        for(int j = 0; j<= max_j; j++){
+            vec2 neighbor_2D = bl_corner + vec2(i*(1/tex_size.x), j*(1/tex_size.y));
 
             //getting the neighbors height
             float height = texture(height_tex, neighbor_2D).x;
@@ -26,26 +28,32 @@ void main() {
             //getting the position of the neighbor
             vec3 neighbor_3D = vec3(neighbor_2D.x, neighbor_2D.y, height);
             normal_mv += neighbor_3D;
-
         }
     }
 
-
     normal_mv = normal_mv/(max_i*max_j);
-
     //normal_mv = vec3(uv.x, uv.y, texture(height_tex, uv).x);
     vec3 n = normalize(normal_mv);
 
-    vec3 light_dir = (n - CAMERA_POSITION);
+    vec3 cam = normalize(CAMERA_POSITION);
+    vec3 light_dir = (n - cam);
+    //vec3 light_dir = (n - CAMERA_POSITION);
     vec3 l = normalize(light_dir);
-
-
 
     //Now we do the difuse shading
     float temp;
     float nl = ((temp = dot(n,l)) < 0)             ? 0.0f : temp;
-    vec3 diffuse = kd * nl * Ld;
+    if(normal_mv.z < 0.33f){
+        //yellow
+        Ld = vec3(1.0f, 1.0f, 0.0f);
+        kd = vec3(0.9f, 0.9f, 0.0f);
+    }else if(normal_mv.z < 0.66f){
+        //green
+        Ld = vec3(0.0f, 1.0f, 0.0f);
+        kd = vec3(0.0f, 1.0f, 0.0f);
 
-    color = (diffuse).xyz;
+    }
+        vec3 diffuse = kd * nl * Ld;
 
+    color = diffuse.xyz;
 }
