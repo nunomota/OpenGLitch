4a48f3c56a2e8a9d1b76c0502696ff1cbcd80680
nunomota <nuno.goncalves@epfl.ch>
2016-04-27 12:43:33 +0200

    Makes Reported static

--- project/Framework/Debugging/Reporter/Reporter.h
+++ project/Framework/Debugging/Reporter/Reporter.h
@@ -9,47 +9,44 @@
   */
 class Reporter {
     private:
-        GlmStrings glm_strings_;
+        static GlmStrings glm_strings_;
 
-        const std::string default_flag_ = "Info";
-        const char flag_delimiters[2]{'[', ']'};
-
-        void output(std::string message, std::string flag) {
+        static void output(std::string message, std::string flag) {
             std::ostringstream final_message;
-            final_message << flag_delimiters[0] << flag << flag_delimiters[1] << " " << message;
+            final_message << '[' << flag << ']' << " " << message;
             std::cout << final_message.str() << std::endl;
         }
 
     public:
-        void println(std::string message, std::string flag) {
+        static void println(std::string message, std::string flag) {
             output(message, flag);
         }
 
-        void println(glm::vec3 vector, std::string flag) {
+        static void println(glm::vec3 vector, std::string flag) {
             output(glm_strings_.create(vector), flag);
         }
 
-        void println(glm::mat4 matrix, std::string flag) {
+        static void println(glm::mat4 matrix, std::string flag) {
             output(glm_strings_.create(matrix), flag);
         }
 
-        void println(std::string message) {
-            output(message, default_flag_);
+        static void println(std::string message) {
+            output(message, "Info");
         }
 
-        void println(glm::vec3 vector) {
-            output(glm_strings_.create(vector), default_flag_);
+        static void println(glm::vec3 vector) {
+            output(glm_strings_.create(vector), "Info");
         }
 
-        void println(glm::mat4 matrix) {
-            output(glm_strings_.create(matrix), default_flag_);
+        static void println(glm::mat4 matrix) {
+            output(glm_strings_.create(matrix), "Info");
         }
 
-        void print_special(std::string message) {
+        static void print_special(std::string message) {
             std::cout << message << std::endl;
         }
 
-        void new_line() {
+        static void new_line() {
             std::cout << std::endl;
         };
 };
