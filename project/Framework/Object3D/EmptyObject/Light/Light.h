d65084cea791e63f5edec14ff34d85c441ad51b3
nunomota <nuno.goncalves@epfl.ch>
2016-05-03 16:30:04 +0200

    Adds getters and setters for simple shading

--- project/Framework/Object3D/EmptyObject/Light/Light.h
+++ project/Framework/Object3D/EmptyObject/Light/Light.h
@@ -3,23 +3,47 @@
 #include "glm/gtc/type_ptr.hpp"
 
 class Light: public EmptyObject {
+    private:
+        static const glm::vec3 default_color;
+
     protected:
-        glm::vec3 color_ = glm::vec3(0.0f, 0.0f, 0.0f);
-        float intensity_ = 1.0f;
+        glm::vec3 ambience_color;
+        glm::vec3 diffuse_color;
+        glm::vec3 specular_color;
+
     public:
+        Light () {
+            setColor(default_color);
+        }
+
+        void setAmbienceColor(glm::vec3 new_color) {
+            ambience_color(new_color);
+        }
+
+        void setDiffuseColor(glm::vec3 new_color) {
+            diffuse_color(new_color);
+        }
+
+        void setSpecularColor(glm::vec3 new_color) {
+            specular_color(new_color);
+        }
+
         void setColor(glm::vec3 new_color) {
-            color_ = new_color;
+            setAmbienceColor(new_color);
+            setDiffuseColor(new_color);
+            setSpecularColor(new_color);
         }
 
-        void setIntensity(float new_intesity) {
-            intensity_ = new_intesity;
+        glm::vec3 getAmbienceColor() {
+            return ambience_color;
         }
 
-        glm::vec3 getColor() {
-            return color_;
+        glm::vec3 getDiffuseColor() {
+            return diffuse_color;
         }
 
-        float getIntensity() {
-            return intensity_;
+        glm::vec3 getSpecularColor() {
+            return specular_color;
         }
 };
+const glm::vec3 default_color = glm::vec3(1.0f, 1.0f, 1.0f);
