94873dbad66f8c82bb399677c7c2a0ab4154a9f2
nunomota <nuno.goncalves@epfl.ch>
2016-05-09 19:56:12 +0200

    Adds methods to bind/unbind a Camera's framebuffer

--- project/Framework/Object3D/EmptyObject/Camera/Camera.h
+++ project/Framework/Object3D/EmptyObject/Camera/Camera.h
@@ -88,6 +88,15 @@ class Camera: public EmptyObject {
             }
         }
 
+        void bindRenderBuffer() {
+            framebuffer.bind();
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        }
+
+        void unbindRenderBuffer() {
+            framebuffer.unbind();
+        }
+
         glm::mat4 getViewMatrix() {
             // TODO maybe calculate this matrix from Transform.getModelMatrix()?
             return transform.getInvertedModelMatrix();
