29f63b24773914715fcd177a411bdf4278db2494
nunomota <nuno.goncalves@epfl.ch>
2016-05-09 15:35:48 +0200

    Adds Loader class for ease of Texture loading

---
+++ project/Framework/Loader/Loader.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "icg_helper.h"
+#include "glm/gtc/type_ptr.hpp"
+
+class Loader {
+    public:
+        static GLuint loadTexture(string texture_name) {
+               int width;
+                int height;
+                int nb_component;
+                GLuint texture_id;
+
+                // set stb_image to have the same coordinates as OpenGL
+                stbi_set_flip_vertically_on_load(1);
+                unsigned char* image = stbi_load(texture_name.c_str(), &width,
+                                                 &height, &nb_component, 0);
+
+                if(image == nullptr) {
+                    throw(string("Failed to load texture"));
+                }
+
+                glGenTextures(1, &texture_id);
+                glBindTexture(GL_TEXTURE_2D, texture_id);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+                if(nb_component == 3) {
+                    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0,
+                                 GL_RGB, GL_UNSIGNED_BYTE, image);
+                } else if(nb_component == 4) {
+                    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
+                                 GL_RGBA, GL_UNSIGNED_BYTE, image);
+                }
+
+                // cleanup
+                glBindTexture(GL_TEXTURE_2D, 0);
+                stbi_image_free(image);
+
+                return texture_id;
+        }
+};
