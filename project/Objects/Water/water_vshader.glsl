91ca64b8916fbd93cb618b14016011927c8729a5
nunomota <nuno.goncalves@epfl.ch>
2016-05-11 11:53:13 +0200

    Fixes inverted texture

--- project/Objects/Water/water_vshader.glsl
+++ project/Objects/Water/water_vshader.glsl
@@ -7,7 +7,7 @@ in vec3 position;
 out vec2 uv;
 
 void main() {
-    uv = (vec2(position.x, position.z) + vec2(1.0, 1.0)) * 0.5;
+    uv = (vec2(position.x, -position.z) + vec2(1.0, 1.0)) * 0.5;
 
     gl_Position = MVP * vec4(position, 1.0);
 }
