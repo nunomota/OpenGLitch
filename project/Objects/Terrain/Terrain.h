76f8e0d62cbe3e39c4ef3ee82e19ffa343c9e567
nunomota <nuno.goncalves@epfl.ch>
2016-05-11 17:57:01 +0200

    Finishes Terrain class

--- project/Objects/Terrain/Terrain.h
+++ project/Objects/Terrain/Terrain.h
@@ -3,18 +3,29 @@
 class Terrain: public Grid {
     private:
         FrameBuffer framebuffer;
+        HeightMap height_map;
         GLuint height_map_id_;
 
     protected:
         void InitialCalculations() {
             Grid::InitialCalculations();
             height_map_id_ = framebuffer.Init(getDimension(), getDimension());
+
+            // Draw the heightmap and store id
+            framebuffer.Bind();
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            HeightMap.Draw();
+            framebuffer.Unbind();
         }
 
         void LoadShaders() {
             // compile the shaders.
             program_id_ = icg_helper::LoadShaders("terrain_vshader.glsl",
                                                   "terrain_fshader.glsl");
+        }
+
+        void SetupUniforms() {
+            addTexture(height_map_id_);
         }
 
         void FinalOperations() {
