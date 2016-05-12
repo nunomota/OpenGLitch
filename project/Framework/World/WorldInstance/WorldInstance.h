8eda0b086c37e1e0e1714ec950180d626447bcb5
nunomota <nuno.goncalves@epfl.ch>
2016-05-12 17:43:28 +0200

    Adds minimap to worldInstance

--- project/Framework/World/WorldInstance/WorldInstance.h
+++ project/Framework/World/WorldInstance/WorldInstance.h
@@ -11,6 +11,8 @@ class WorldInstance: public World {
         Terrain* terrain;
         Water* water;
 
+        LiveViewer* minimap;
+
     protected:
 
         // method called only once
@@ -18,50 +20,48 @@ class WorldInstance: public World {
             Reporter::println("Start method called");
             camera = getCamera();
             camera2 = instantiate(new Camera(45.0f, 1.0f, 0.1f, 100.0f));
+            enableLiveRenderer(camera2);
 
             terrain = instantiate(new Terrain());
             water = instantiate(new Water());
+            minimap = instantiate(new LiveViewer(camera2->getRenderTextureID()));
 
             camera->translate(vec3(0.0f, 0.0f, 8.0f));
             camera->scale(vec3(-0.2f, -0.2f, -0.2f));
             
-            //camera2->translate(vec3(0.0f, 3.0f, 0.0f));
             camera2->rotate(vec3(-90.0f, 0.0f, 0.0f));
+            camera2->translate(vec3(0.0f, 0.0f, 3.0f));
 
-            //terrain->rotate(vec3(90.0f, 0.0f, 0.0f));
-            //water->translate(vec3(0.5f, 0.0f, 0.0f));
-            //water->rotate(vec3(90.0f, 0.0f, 0.0f));
+            minimap->rotate(vec3(90.0f, 0.0f, 0.0f));
+            minimap->translate(vec3(0.0f, 0.0f, 6.0f));
+            minimap->scale(vec3(0.0f, 0.5f, 0.0f));
         }
 
         // method called every frame
         void Update() {
             // forward/backward camera movement
             if (getKeyDown(Keyboard::W)) {
-                camera->translate(vec3(0.0f, 0.0f, -1.0f) * getTime()->getDeltaTime());
+                getCamera()->translate(vec3(0.0f, 0.0f, -1.0f) * getTime()->getDeltaTime());
             } else if (getKeyDown(Keyboard::S)) {
-                camera->translate(vec3(0.0f, 0.0f, 1.0f) * getTime()->getDeltaTime());
+                getCamera()->translate(vec3(0.0f, 0.0f, 1.0f) * getTime()->getDeltaTime());
             }
 
             // sideways camera movement
             if (getKeyDown(Keyboard::D)) {
-                camera->translate(vec3(1.0f, 0.0f, 0.0f) * getTime()->getDeltaTime());
+                getCamera()->translate(vec3(1.0f, 0.0f, 0.0f) * getTime()->getDeltaTime());
             } else if (getKeyDown(Keyboard::A)) {
-                camera->translate(vec3(-1.0f, 0.0f, 0.0f) * getTime()->getDeltaTime());
+                getCamera()->translate(vec3(-1.0f, 0.0f, 0.0f) * getTime()->getDeltaTime());
             }
 
             // upwards/downwards camera movement
             if (getKeyDown(Keyboard::P)) {
-                camera->translate(vec3(0.0f, 1.0f, 0.0f) * getTime()->getDeltaTime());
+                getCamera()->translate(vec3(0.0f, 1.0f, 0.0f) * getTime()->getDeltaTime());
             } else if (getKeyDown(Keyboard::L)) {
-                camera->translate(vec3(0.0f, -1.0f, 0.0f) * getTime()->getDeltaTime());
+                getCamera()->translate(vec3(0.0f, -1.0f, 0.0f) * getTime()->getDeltaTime());
             }
 
             if (getKeyPressed(Keyboard::Z)) {
                 setCamera(camera2);
-            }
-
-            if (getKeyPressed(Keyboard::X)) {
-                setCamera(camera);
             }
         }    
 };
