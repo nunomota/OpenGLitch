acfc4629e09625924efba05b66b1f7311cbd59c3
nunomota <nuno.goncalves@epfl.ch>
2016-05-14 17:01:01 +0200

    Moves infinite-terrain related code into separate classes for simplicity

--- project/Auxiliary/InfiniteTerrain/Chunk/Chunk.h
+++ project/Auxiliary/InfiniteTerrain/Chunk/Chunk.h
@@ -5,6 +5,11 @@ class Chunk {
         Terrain* terrain;
         Water* water;
 
+        Chunk(Terrain* new_terrain = NULL, Water* new_water = NULL) {
+            terrain = new_terrain;
+            water = new_water;
+        }
+
         void translate(glm::vec3 t_vec) {
             terrain->translate(t_vec);
             water->translate(t_vec);
--- project/Framework/World/WorldInstance/WorldInstance.h
+++ project/Framework/World/WorldInstance/WorldInstance.h
@@ -4,23 +4,6 @@
 
 using namespace glm;
 
-/** The index of a chunk in the array
-  * also represents its relative
-  * position according to all others:
-  *
-  *    [2, 3]
-  *    [0, 1]
-  *
-  * This means that their indexes will 
-  * change based on camera movement.
-  */
-struct {
-    float chunk_width = 2.0f;
-    Chunk chunks[4];
-    int visible_chunk;
-    int last_quadrant;
-} infinite_terrain;
-
 class WorldInstance: public World {
     private:
         Camera* camera;
@@ -28,6 +11,8 @@ class WorldInstance: public World {
 
         Minimap* minimapBg;
         LiveViewer* minimap;
+
+        InfiniteTerrain infinite_terrain = InfiniteTerrain(camera);
 
     protected:
 
@@ -71,7 +56,7 @@ class WorldInstance: public World {
                 getCamera()->translate(-getCamera()->getTransform()->getForwardVector() * getTime()->getDeltaTime());
             }
 
-            updateInfiniteTerrain();
+            infinite_terrain.update();
 
             // make minimap camera follow the main camera
             Transform* camera_transform = camera->getTransform();
@@ -96,126 +81,9 @@ class WorldInstance: public World {
         }
 
         void setupInfiniteTerrain() {
-
-            vec3 camera_position = camera->getTransform()->getPosition();
-            vec3 bl_chunk_position = vec3(camera_position.x, 0.0f, camera_position.z);
-            vec3 forward_vector = vec3(0.0f, 0.0f, -1.0f) * infinite_terrain.chunk_width;
-            vec3 right_vector = vec3(1.0f, 0.0f, 0.0f) * infinite_terrain.chunk_width;
-
-            // instantiate all chunks
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 2; j++) {
-                    Terrain* new_terrain = instantiate(new Terrain());
-                    Transform* terrain_transform = new_terrain->getTransform();
-                    Water* new_water = instantiate(new Water());
-
-                    terrain_transform->setPosition(bl_chunk_position + (float)i*forward_vector + (float)j*right_vector);
-                    new_water->getTransform()->setPosition(terrain_transform->getPosition());
-
-                    infinite_terrain.chunks[i*2 + j].terrain = new_terrain;
-                    infinite_terrain.chunks[i*2 + j].water   = new_water;
-                }
+            for (int i = 0; i < 4; i++) {
+                infinite_terrain.setChunk(i, Chunk(instantiate(new Terrain()), instantiate(new Water())));
             }
-            infinite_terrain.visible_chunk = 0;
-            infinite_terrain.last_quadrant = getQuadrant();
-        }
-
-        void updateInfiniteTerrain() {
-            int cur_quadrant = getQuadrant();
-            if (cur_quadrant != -1) {
-                if (cur_quadrant != infinite_terrain.last_quadrant) {
-                    regroupChunks(infinite_terrain.last_quadrant, cur_quadrant);
-                    infinite_terrain.last_quadrant = cur_quadrant;
-                }
-            } else {
-                // TODO assign new visible chunk
-            }
-        }
-
-        int getQuadrant() {
-            float chunk_width = infinite_terrain.chunk_width;
-            vec3 camera_position = camera->getTransform()->getPosition();
-            vec3 chunk_position  = infinite_terrain.chunks[infinite_terrain.visible_chunk].terrain->getTransform()->getPosition();
-            
-            if (inBounds(camera_position.x, chunk_position.x, chunk_position.x + chunk_width/2.0f) &&
-                inBounds(camera_position.z, chunk_position.z, chunk_position.z + chunk_width/2.0f)) {
-                return 1;
-            } else if (inBounds(camera_position.x, chunk_position.x - chunk_width/2.0f, chunk_position.x) &&
-                       inBounds(camera_position.z, chunk_position.z, chunk_position.z + chunk_width/2.0f)) {
-                return 0;
-            } else if (inBounds(camera_position.x, chunk_position.x, chunk_position.x + chunk_width/2.0f) &&
-                       inBounds(camera_position.z, chunk_position.z - chunk_width/2.0f, chunk_position.z)) {
-                return 3;
-            } else if (inBounds(camera_position.x, chunk_position.x - chunk_width/2.0f, chunk_position.x) &&
-                       inBounds(camera_position.z, chunk_position.z - chunk_width/2.0f, chunk_position.z)) {
-                return 2;
-            } else {
-                return -1;
-            }
-        }
-
-        bool inBounds(float target, float min, float max) {
-            if (target >= min && target <= max) {
-                return true;
-            }
-            return false;
-        }
-
-        void regroupChunks(int last, int current) {
-            vec2 mov_vector = (quadToCoords(current) - quadToCoords(last));
-
-            if (mov_vector.x == 1) {
-                // right
-                infinite_terrain.chunks[0].translate(vec3(infinite_terrain.chunk_width*2.0f, 0.0f, 0.0f));
-                infinite_terrain.chunks[2].translate(vec3(infinite_terrain.chunk_width*2.0f, 0.0f, 0.0f));
-                chunkSwap(0, 1);
-                chunkSwap(2, 3);
-            } else if (mov_vector.x == -1) {
-                // left
-                infinite_terrain.chunks[1].translate(vec3(infinite_terrain.chunk_width*-2.0f, 0.0f, 0.0f));
-                infinite_terrain.chunks[3].translate(vec3(infinite_terrain.chunk_width*-2.0f, 0.0f, 0.0f));
-                chunkSwap(0, 1);
-                chunkSwap(2, 3);
-            }
-
-            if (mov_vector.y == 1) {
-                // up
-                infinite_terrain.chunks[0].translate(vec3(0.0f, 0.0f, infinite_terrain.chunk_width*-2.0f));
-                infinite_terrain.chunks[1].translate(vec3(0.0f, 0.0f, infinite_terrain.chunk_width*-2.0f));
-                chunkSwap(0, 2);
-                chunkSwap(1, 3);
-            } else if (mov_vector.y == -1) {
-                // down
-                infinite_terrain.chunks[2].translate(vec3(0.0f, 0.0f, infinite_terrain.chunk_width*2.0f));
-                infinite_terrain.chunks[3].translate(vec3(0.0f, 0.0f, infinite_terrain.chunk_width*2.0f));
-                chunkSwap(0, 2);
-                chunkSwap(1, 3);
-            }
-        }
-
-        void chunkSwap(int index1, int index2) {
-            // save pointers at index1
-            Terrain* temp_terrain = infinite_terrain.chunks[index1].terrain;
-            Water* temp_water     = infinite_terrain.chunks[index1].water;
-
-            // overwrite pointers at index1
-            infinite_terrain.chunks[index1].terrain = infinite_terrain.chunks[index2].terrain;
-            infinite_terrain.chunks[index1].water   = infinite_terrain.chunks[index2].water;
-
-            // write pointers to index2
-            infinite_terrain.chunks[index2].terrain = temp_terrain;
-            infinite_terrain.chunks[index2].water   = temp_water;
-        }
-
-        vec2 quadToCoords(int quadrant) {
-            if (quadrant == 0) {
-                return vec2(0, 0);
-            } else if (quadrant == 1) {
-                return vec2(1, 0);
-            } else if (quadrant == 2) {
-                return vec2(0, 1);
-            } else {
-                return vec2(1, 1);
-            }
+            infinite_terrain.initialize();
         }
 };
