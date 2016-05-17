f8cfdb11bcfcbcd7aed310ebec6b93da4cea9409
nunomota <nuno.goncalves@epfl.ch>
2016-05-17 19:10:04 +0200

    Edits water shader to display reflection

--- project/Objects/Water/water_fshader.glsl
+++ project/Objects/Water/water_fshader.glsl
@@ -5,7 +5,11 @@ in vec2 uv;
 out vec3 color;
 
 uniform sampler2D tex0;
+uniform sampler2D tex1;
 
 void main() {
-    color = texture(tex0, uv).rgb;
+    vec4 main_texture = texture(tex0, uv);
+    vec4 reflection = texture(tex1, uv);
+    color = mix(main_texture, reflection, main_texture.a).rgb;
+    color = texture(tex1, uv).rgb;
 }
