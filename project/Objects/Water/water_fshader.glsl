2cbffbb167a4255656ed15b0a0e90b0050897e62
nunomota <nuno.goncalves@epfl.ch>
2016-05-18 12:17:52 +0200

    Minor testing in water shaders

--- project/Objects/Water/water_fshader.glsl
+++ project/Objects/Water/water_fshader.glsl
@@ -1,7 +1,7 @@
 #version 330
 
 in vec2 uv;
-in vec4 clipSpace;
+in vec4 gl_FragCoord;
 
 out vec3 color;
 
@@ -9,10 +9,11 @@ uniform sampler2D tex0;
 uniform sampler2D tex1;
 
 void main() {
-    vec2 ndc = (clipSpace.xz/clipSpace.w)/2.0f + 0.5f;
+    ivec2 window_size = textureSize(tex1, 0);
 
-    vec2 reflectTexCoords = vec2(ndc.x, -ndc.y);
+    float _u = (gl_FragCoord.x - 0.5f) / window_size.x;
+    float _v = (gl_FragCoord.y - 0.5f) / window_size.y;
 
-    //color = mix(texture(tex0, uv).rgb, texture(tex1, vec2(_u, _v)).rgb, vec3(0.15));
-    color = mix(texture(tex1, reflectTexCoords).rgb, texture(tex0, uv).rgb, vec3(0.5));
+    color = mix(texture(tex0, uv).rgb, texture(tex1, vec2(_u, _v)).rgb, vec3(0.15));
+    color = texture(tex1, vec2(_u, _v)).rgb;
 }
--- project/Objects/Water/water_vshader.glsl
+++ project/Objects/Water/water_vshader.glsl
@@ -5,12 +5,9 @@ uniform mat4 MVP;
 in vec3 position;
 
 out vec2 uv;
-out vec4 clipSpace;
 
 void main() {
     uv = (vec2(position.x, -position.z) + vec2(1.0, 1.0)) * 0.5;
 
-    clipSpace = MVP * vec4(position, 1.0);
-
-    gl_Position = clipSpace;
+    gl_Position = MVP * vec4(position, 1.0);
 }
