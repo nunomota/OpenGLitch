2e4e509912d8677accce29a24f3d8e3dc7fab824
nunomota <nuno.goncalves@epfl.ch>
2016-05-28 16:21:22 +0200

    Fixes Auxiliary.h merging

--- project/Auxiliary/Auxiliary.h
+++ project/Auxiliary/Auxiliary.h
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-#include "InfiniteTerrain/Chunk/Chunk.h"
-#include "InfiniteTerrain/InfiniteTerrain.h"
-
 #include "Mirror/Mirror.h"
 #include "Controller/Controller.h"
-=======
-#include "Mirror/Mirror.h"
->>>>>>> 82e975a36594ea49defc1695d73c70945226bdc9
