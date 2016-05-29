5612603750ba0dbede0118bead3a709b067c6440
nunomota <nuno.goncalves@epfl.ch>
2016-05-29 15:04:51 +0200

    Changes method signature

--- project/Framework/Loader/Loader.h
+++ project/Framework/Loader/Loader.h
@@ -40,7 +40,10 @@ class Loader {
             return texture_id;
         }
 
-        static bool loadObj(string filename) {
+        static bool loadObj(const char * path,
+                            std::vector < glm::vec3 > & out_vertices,
+                            std::vector < glm::vec2 > & out_uvs,
+                            std::vector < glm::vec3 > & out_normals) {
             return false;
         }
 };
