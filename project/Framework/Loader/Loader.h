68c3b7ac9fdab9b6b2a8a489110531b6ff8275ce
nunomota <nuno.goncalves@epfl.ch>
2016-05-29 14:47:18 +0200

    Fixes Loader syntax

--- project/Framework/Loader/Loader.h
+++ project/Framework/Loader/Loader.h
@@ -5,41 +5,38 @@
 class Loader {
     public:
         static GLuint loadTexture(string texture_name) {
-               int width;
-                int height;
-                int nb_component;
-                GLuint texture_id;
+            int width;
+            int height;
+            int nb_component;
+            GLuint texture_id;
 
-                // set stb_image to have the same coordinates as OpenGL
-                stbi_set_flip_vertically_on_load(1);
-                unsigned char* image = stbi_load(texture_name.c_str(), &width,
-                                                 &height, &nb_component, 0);
+            // set stb_image to have the same coordinates as OpenGL
+            stbi_set_flip_vertically_on_load(1);
+            unsigned char* image = stbi_load(texture_name.c_str(), &width,
+                                             &height, &nb_component, 0);
 
-                if(image == nullptr) {
-                    throw(string("Failed to load texture"));
-                }
+            if(image == nullptr) {
+                throw(string("Failed to load texture"));
+            }
 
-                glGenTextures(1, &texture_id);
-                glBindTexture(GL_TEXTURE_2D, texture_id);
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-                glEnable(GL_BLEND);
+            glGenTextures(1, &texture_id);
+            glBindTexture(GL_TEXTURE_2D, texture_id);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glEnable(GL_BLEND);
 
-                if(nb_component == 3) {
-                    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0,
-                                 GL_RGB, GL_UNSIGNED_BYTE, image);
-                } else if(nb_component == 4) {
-                    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
-                                 GL_RGBA, GL_UNSIGNED_BYTE, image);
-                }
-				
-				
-				
-				
-                // cleanup
-                glBindTexture(GL_TEXTURE_2D, 0);
-                stbi_image_free(image);
+            if(nb_component == 3) {
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0,
+                             GL_RGB, GL_UNSIGNED_BYTE, image);
+            } else if(nb_component == 4) {
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
+                             GL_RGBA, GL_UNSIGNED_BYTE, image);
+            }
 
-                return texture_id;
+            // cleanup
+            glBindTexture(GL_TEXTURE_2D, 0);
+            stbi_image_free(image);
+
+            return texture_id;
         }
 };
