4ca1a876c27db83cbff6a564b5bf07b971eaa2de
nunomota <nuno.goncalves@epfl.ch>
2016-05-29 20:18:23 +0200

    Fixes variable type

--- project/Framework/Object3D/PhysicalObject/CustomObject/custom_object_fshader.glsl
+++ project/Framework/Object3D/PhysicalObject/CustomObject/custom_object_fshader.glsl
@@ -1,6 +1,6 @@
 #version 330
 
-uniform Sampler2D tex0;
+uniform sampler2D tex0;
 
 in vec2 uv;
 in vec3 normal;
@@ -8,5 +8,5 @@ in vec3 normal;
 out vec3 color;
 
 void main() {
-    color = texture(tex0, uv);
+    color = texture(tex0, uv).rgb;
 }
